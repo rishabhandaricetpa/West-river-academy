4f4c71c3d75421cb83d2419a67f0f2d81dbae6ce
Paige Priyanka <paige.priyanka@ithands.com>
2020-11-19 17:57:22 +0530

    revised user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();;
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('country');
+            $table->string('immunized')->nullable();
+            $table->string('reference')->nullable();
+            $table->tinyInteger('verified_status');
             $table->rememberToken();
             $table->timestamps();
         });
