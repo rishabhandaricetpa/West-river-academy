af72cc3bd04f0c1073f6731c1fbbcef610ffdce3
Nancy Shalini <nancy.shalini@ithands.biz>
2020-11-25 16:26:12 +0530

    Add login page

---
+++ resources/js/custom.js
@@ -0,0 +1,12 @@
+
+  /* Bootstrap dropdown on hover */
+  $(".dropdown-menu").css('margin-top', 0);
+  $(".dropdown")
+  .mouseover(function () {
+    $(this).addClass('show').attr('aria-expanded', "true");
+    $(this).find('.dropdown-menu').addClass('show');
+  })
+  .mouseout(function () {
+    $(this).removeClass('show').attr('aria-expanded', "false");
+    $(this).find('.dropdown-menu').removeClass('show');
+  });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
 
 <body>
   <div id="app" class="app">
-    <img src="/resources/images/gradient-bg.jpg" class="img-absolute" alt="bg-img">
+    <img src="https://www.westriveracademy.com/cwp/img/banner.jpg" class="img-absolute" alt="bg-img">
     <!-- * =============== Header =============== * -->
 <header class="site-header">
   <nav class="navbar navbar-expand-lg navbar-dark">
@@ -84,15 +84,25 @@
 
     <!-- * =============== /Main =============== * -->
 
-    <footer class="bg-dark position-relative">
+    <footer class="bg-dark position-relative text-center main-footer">
       <div class="container">
-        <ul class="social-icons list-unstyled">
-          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
-          <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-          <li><a href="#"><i class="fab fa-instagram-square"></i></a></li>
+        <ul class="social-icons list-unstyled d-flex justify-content-center">
+          <li><a href="#" class="mx-2"><i class="fab fa-facebook"></i></a></li>
+          <li><a href="#" class="mx-2"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="#" class="mx-2"><i class="fab fa-pinterest"></i></a></li>
+          <li><a href="#" class="mx-2"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="#" class="mx-2"><i class="fab fa-instagram-square"></i></a></li>
         </ul>
+        <a href="#" role="button" class="btn btn-secondary text-uppercase mt-4 mb-3">contact us</a>
+        <ul class="list-unstyled">
+        <li><a href=""><i class="fas fa-phone-alt mr-2"></i>949 - 492 - 5240</a></li>
+        <li><a href="#"><i class="fas fa-envelope mr-2"></i>contact@westriveracademy.com</a></li>
+        </ul>
+        <div class="copyright">
+	       	Copyright © 2020 West River Academy. 
+          <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>.
+	        <a href="#" class="d-block">Website design, development and maintenance by eXcelisys, Inc.</a>
+	</div>
       </div>
     </footer>
   </div>
