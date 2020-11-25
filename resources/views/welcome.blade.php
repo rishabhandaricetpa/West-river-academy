485f1da2f70949e82593728125dbcdda33ad525a
Nancy Shalini <nancy.shalini@ithands.biz>
2020-11-25 15:44:09 +0530

    basic login form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,113 @@
-@extends('layouts.app')
-@section('pageTitle', 'Welcome')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-  <div class="container">
-    <h1 class="text-center py-5">Welcome !</h1>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="robots" content="noindex, nofollow">
+  <title> West River Academy </title>
+  <!-- Style Files -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <link href="css/app.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+  <div id="app" class="app">
+    <img src="/resources/images/gradient-bg.jpg" class="img-absolute" alt="bg-img">
+    <!-- * =============== Header =============== * -->
+<header class="site-header">
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand" href="/"><img src="images/wra_logo.svg" alt="wra_logo"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-header" aria-controls="main-header" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="main-header">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Enrollment</a>
+          <ul class="dropdown-menu" aria-labelledby="dropdown05">
+            <li><a class="dropdown-item" href="#">international</a></li>
+            <li> <a class="dropdown-item" href="#">graduation</a></li>
+           <li>  <a class="dropdown-item" href="#">Accreditation</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">our story <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">faq</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <a href="#" role="button" class="btn btn-primary">enroll now</a>
+     <a href="#" class="user-login"><img class="o-contain" src="images/login-img.png" alt=""></a>
+    </div>
+  </nav>
+</header>
+    <!-- * =============== /Header =============== * -->
+
+    <!-- * =============== Main =============== * -->
+  <main class="position-relative container">
+    <div class="form-wrap border bg-light">
+      <h2>Login to My Account</h2>
+    <form>
+      <div class="form-group d-flex mb-1">
+        <label for="exampleInputEmail1">User ID</label>
+        <div>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div class="alert alert-danger">Enter your user ID.</div>
+      </div>
+      </div>
+      <div class="form-group d-flex mb-1">
+        <label for="exampleInputPassword1">Password</label>
+        <div>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+        <div class="alert alert-danger">Enter your password.</div>
+      </div>
+      </div>
+      <div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+        <a href="#" class="ml-4">Forgot Password?</a>
+    </div>
+    <div class="register-info">Don't have an account yet? <a href="#">Click this link to create one.</a></div>
+    </form>
   </div>
-@endsection
+  </main>
+
+    <!-- * =============== /Main =============== * -->
+
+    <footer class="bg-dark position-relative">
+      <div class="container">
+        <ul class="social-icons list-unstyled">
+          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+          <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="#"><i class="fab fa-instagram-square"></i></a></li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="js/app.js" async></script>
+   <!-- Footer scripts -->
+ @stack('before_vendor_scripts')
+<script defer src="{{ mix('js/manifest.js') }}"></script>
+<script defer src="{{ mix('js/vendor.js') }}"></script>
+@stack('after_vendor_scripts')
+@stack('before_app_scripts')
+<script defer async src="{{ mix('js/app.js') }}"></script>
+@stack('after_app_scripts')
+</body>
+
+</html>
+
+
