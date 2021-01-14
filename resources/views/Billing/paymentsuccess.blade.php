08e4da98c82c386d0ab9d220455cf560fb91f513
paige.priyanka <paige.priyanka@ithands.com>
2021-01-14 17:42:46 +0530

    minor fixes on stripe email receipt

--- resources/views/Billing/paymentsuccess.blade.php
+++ resources/views/Billing/paymentsuccess.blade.php
@@ -5,10 +5,9 @@
 
 <main class="position-relative container form-content mt-4">
   <div class="form-wrap border bg-light form-content small-container">
-  <!-- <form method="post" action="{{route('payment.info',$charges->id)}}"> -->
     @csrf
     <h2 class="text-capitalize">Thank you!</h2>
-    <p class="mb-0">Your Transaction with Stripe is successfull.Your order id is Transaction Id: {{$charges->id}}
+    <p class="mb-0">Your Transaction with Stripe is successfull.Please check Your Email for Transaction Receipt!!
     <a href="{{route('dashboard')}}" class="btn btn-primary">Go to Dashboard</a>
 
     </p>
