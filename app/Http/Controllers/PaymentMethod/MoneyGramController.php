436c13aaa864d2cc17c7a30bae03e9173df8214d
Trevor Krishan <trevorkrishan@trevorkrishan.local>
2021-02-04 15:12:34 +0530

    added coupon details on other payments

--- app/Http/Controllers/PaymentMethod/MoneyGramController.php
+++ app/Http/Controllers/PaymentMethod/MoneyGramController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\PaymentMethod;
+
 use App\Http\Controllers\Controller;
 use App\Models\TransactionsMethod;
 use Illuminate\Http\Request;
@@ -29,40 +30,46 @@ class MoneyGramController extends Controller
     /**
      * payment view.
      */
-    public function index(){
-       $id =Auth::user()->id;
-       $user=  User::find($id)->first();
-       $email= Auth::user()->email;
-     //update cart status active 
-     $address = User::find($id)->parentProfile()->first();
-     $date = \Carbon\Carbon::now()->format('Y-m-d');
-     $parentProfileData = User::find($id)->parentProfile()->first();
-     $enroll_fees= Cart::getCartAmount($this->parent_profile_id,true);
+    public function index()
+    {
+        $id = Auth::user()->id;
+        $user =  User::find($id)->first();
+        $email = Auth::user()->email;
+        //update cart status active 
+        $address = User::find($id)->parentProfile()->first();
+        $date = \Carbon\Carbon::now()->format('Y-m-d');
+        $parentProfileData = User::find($id)->parentProfile()->first();
+        
+        $enroll_fees = Cart::getCartAmount($this->parent_profile_id, true);
+        $coupon_code = session('applied_coupon', null);
+        $coupon_amount = session('applied_coupon_amount', 0);
+        $amount = $enroll_fees->amount;
+        $final_amount = $coupon_amount > $amount ? 0 : $amount - $coupon_amount;
 
-     $paymentinfo = new TransactionsMethod;
-     $paymentinfo = $parentProfileData->TransactionsMethod()->create([
-      'parent_profile_id'=>$parentProfileData,
-      'transcation_id' => substr(uniqid(), 0, 8),
-      'payment_mode'=>'Money Gram',
-      'amount'=>$enroll_fees->amount ,
-      'status'=>'active',
-      ]);
-      
-       $cartItems=Cart::select('item_id')->where('parent_profile_id',$id)->get();
-       foreach ($cartItems as $cart) 
-       {
-         $enrollemtpayment=EnrollmentPayment::select()->where('enrollment_period_id',$cart->item_id)->first();
-         $enrollemtpayment->status='active';
-         $enrollemtpayment->payment_mode='MoneyGram';
-         $enrollemtpayment->save();
-       }
+        $paymentinfo = new TransactionsMethod;
+        $paymentinfo = $parentProfileData->TransactionsMethod()->create([
+            'parent_profile_id' => $parentProfileData,
+            'transcation_id' => substr(uniqid(), 0, 8),
+            'payment_mode' => 'Money Gram',
+            'amount' => $amount,
+            'status' => 'active',
+            'coupon_code' => $coupon_code,
+            'coupon_amount' => $coupon_amount,
+        ]);
 
-       $refreshCart=Cart::select()->where('parent_profile_id',$id)->get();
-       $refreshCart->each->delete();
+        $cartItems = Cart::select('item_id')->where('parent_profile_id', $id)->get();
+        foreach ($cartItems as $cart) {
+            $enrollemtpayment = EnrollmentPayment::select()->where('enrollment_period_id', $cart->item_id)->first();
+            $enrollemtpayment->status = 'active';
+            $enrollemtpayment->payment_mode = 'MoneyGram';
+            $enrollemtpayment->save();
+        }
+
+        $refreshCart = Cart::select()->where('parent_profile_id', $id)->get();
+        $refreshCart->each->delete();
 
         Mail::to($email)->send(new MoneyGram($user));
 
         return view('mail.moneygram-review', compact('email', 'date'));
     }
-    
 }
