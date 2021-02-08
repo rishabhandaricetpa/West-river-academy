4e59ed1847a8a22174237b681253703bd7b2984e
Nancy Shalini <nancy.shalini@ithands.biz>
2021-02-08 14:32:00 +0530

    fixes for admin sidebar

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -78,7 +78,7 @@
     </li>
     <li class="nav-item">
       <a href="{{route('admin.logout')}}" class="nav-link">
-        <i class="nav-icon fas fa-copy"></i>
+      <i class="fas fa-sign-out-alt"></i>
         <p>
           Logout
         </p>
