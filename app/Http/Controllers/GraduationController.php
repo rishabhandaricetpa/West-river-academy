dce98c9520e612ec619a517efed3ea523ba8cb8a
Trevor Krishan <trevorkrishan@trevorkrishan.local>
2021-02-08 11:03:01 +0530

    yarn changes

--- app/Http/Controllers/GraduationController.php
+++ app/Http/Controllers/GraduationController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Auth;
+use App\Models\ParentProfile;
+use App\Models\StudentProfile;
 
 class GraduationController extends Controller
 {
-    //
+    public function index()
+    {
+        // $students = StudentProfile::
+    }
 }
