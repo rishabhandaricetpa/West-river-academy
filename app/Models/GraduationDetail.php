4161fd8e0bcdd7075bcc4ffebcde43a732206580
Trevor Krishan <trevorkrishan@trevorkrishan.local>
2021-02-09 15:52:10 +0530

    added graduation details

---
+++ app/Models/GraduationDetail.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GraduationDetail extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'graduation_id', 'project', 'diploma', 'transcript', 'situation', 'record_received', 'grad_date', 'apostille_package', 'notes'
+    ];
+}
