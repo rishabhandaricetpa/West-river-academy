f9fc5d57b4189d9f7d942609cc156f2fc07d9911
trevor.krishan <trevor.krishan@ithands.com>
2021-03-01 12:44:01 +0530

    added mailing after graduation request approveed

---
+++ resources/views/mail/graduation-approved.blade.php
@@ -0,0 +1,7 @@
+<p>Hi,</p>
+
+<p>We have reviewed your application to graduate and approved it. Please log in to your account and pay the Graduation fee of ${{ $data->total_fee }}, which includes the Graduation Project, high school diploma, {{ $data->message }} </p>
+
+<p>We look forward to working with you and helping you graduate from West River Academy.</p>
+
+<a href="{{ route('login') }}">Log In</a>
