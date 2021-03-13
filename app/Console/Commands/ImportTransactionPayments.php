fe8b5e6b7e98d206148cb1e7beb9da307a45dbd6
paige.priyanka <paige.priyanka@ithands.com>
2021-03-13 22:09:53 +0530

    migrated transction method data

--- app/Console/Commands/ImportTransactionPayments.php
+++ app/Console/Commands/ImportTransactionPayments.php
@@ -6,24 +6,25 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use App\Models\EnrollmentPayment;
-use App\Models\EnrollmentPeriods;
+use App\Models\ParentProfile;
+use App\Models\TransactionsMethod;
 use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 
-class ImportPaymentForEnrollments extends Command
+class ImportTransactionPayments extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'import:payments';
+    protected $signature = 'import:transactions';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Import Payment For Enrollments';
+    protected $description = 'Import transcations data';
 
     /**
      * Create a new command instance.
@@ -43,7 +44,7 @@ class ImportPaymentForEnrollments extends Command
     public function handle()
     {
         $this->line('starting import');
-        $filePath = base_path('csv/paymentsDataforenrollment.csv');
+        $filePath = base_path('csv/payments.csv');
         $reader = ReaderEntityFactory::createReaderFromFile($filePath);
         $reader->open($filePath);
 
@@ -54,21 +55,18 @@ class ImportPaymentForEnrollments extends Command
                 if ($rowIndex === 1) {
                     continue;
                 }
-                $order_id = Str::of($cells[19]);
-                $payment_order_id = EnrollmentPayment::where('order_id', $order_id)->first();
-                if ($payment_order_id) {
-                    // $student = EnrollmentPayment::where('order_id', $order_id);
-                    // $student->transcation_id = $cells[15];
-                    // $student->payment_mode =  $cells[17];
-                    // $student->update();
-                    $clearpendingPayments = EnrollmentPayment::where('order_id', $order_id)->update(
-                        [
-                            'transcation_id' => $cells[15],
-                            'payment_mode' => $cells[17],
-                        ]
-                    );
-                } else {
-                    continue;
+                $legacy_name = Str::of($cells[12]);
+                $email = Str::of($cells[11]);
+                $parent_profile = ParentProfile::where('p1_email', $email)->first();
+                if ($parent_profile) {
+                    TransactionsMethod::create([
+                        'transcation_id' => $cells[15],
+                        'payment_mode' => $cells[17],
+                        'order_id' => $cells[19],
+                        'parent_profile_id' => (isset($parent_profile)) ? $parent_profile->id : 0,
+                        'amount' => $cells[20],
+                        'status' => $cells[16],
+                    ]);
                 }
             }
         }
