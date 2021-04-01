eac6c5f8a0e8b155577e4368ff55bce7e97b82ad
spencer <spencer.suraj@ithands.biz>
2021-04-01 15:06:13 +0530

    update: preview-transcript - showing courses and their subjects information in tabular format - calculated total Credit score

--- config/constants.php
+++ config/constants.php
@@ -9,4 +9,9 @@ return [
   'ENROLLMENTPERIODS' => ['1' => 'ANNUAL', '2' => 'SEMESTER'],
   'RCERTIFIED' => ['UnderTraining' => 1, 'Invited' => 0, 'Certified' => 2],
 
+  'GRADES' => [
+    "9_10" => [ 9, 10 ],
+    "11_12" => [ 11, 12 ],
+   ],
+
 ];
