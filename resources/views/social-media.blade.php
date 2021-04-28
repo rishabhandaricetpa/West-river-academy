02c31ee8702944b9485e709e9903a7cc476debd0
Nancy Shalini <nancy.shalini@ithands.biz>
2021-04-28 10:35:03 +0530

    add social media page

--- resources/views/social-media.blade.php
+++ resources/views/social-media.blade.php
@@ -2,9 +2,68 @@
 @section('pageTitle', 'Transcript Information')
 @section('content')
 <!-- * =============== Main =============== * -->
-<main class="position-relative container form-content mt-4">
-    <h1>Hiii</h1>
+<div class="content-wrapper">
+            <main class="position-relative container form-content mt-4">
+  <h1 class="text-center text-white text-uppercase">join our community</h1>
+  <div class="form-wrap border bg-light py-5 px-25">
+  <ul class="list-unstyled social-community">
+  <li class="media mb-4">
+      <div>
+  <img src="/images/MeWe.png" class="o-contain" alt="MeWe">
+  <h3 class="text-center">MEWE</h3>
+  </div>
+    <div class="media-body pl-2 pt-2 pl-lg-5 pt-lg-5">
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media mb-4">
+      <div>
+      <img src="/images/Instagram.png" class="o-contain" alt="Instagram">
+  <h3 class="text-center">Instagram</h3>
+  </div>
+    <div class="media-body pl-2 pt-2 pl-lg-5 pt-lg-5">
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media mb-4">
+      <div>
+      <img src="/images/Pinterest.png" class="o-contain" alt="Pinterest">
+  <h3 class="text-center">Pinterest</h3>
+  </div>
+    <div class="media-body pl-2 pt-2 pl-lg-5 pt-lg-5">
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media mb-4">
+      <div>
+      <img src="/images/Facebook.png" class="o-contain" alt="facebook">
+  <h3 class="text-center">Facebook</h3>
+  </div>
+    <div class="media-body pl-2 pt-2 pl-lg-5 pt-lg-5">
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media mb-4">
+      <div>
+      <img src="/images/Blog.png" class="o-contain" alt="blog">
+  <h3 class="text-center">Blog</h3>
+  </div>
+    <div class="media-body pl-2 pt-2 pl-lg-5 pt-lg-5">
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media mb-4">
+      <div>
+      <img src="/images/Microphone.png" class="o-contain" alt="Microphone">
+  <h3 class="text-center">Microphone</h3>
+  </div>
+    <div class="media-body pl-2 pt-2 pl-lg-5 pt-lg-5">
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+</ul>
+    </div>
 </main>
-
+        </div>
 <!-- * =============== /Main =============== * -->
 @endsection
