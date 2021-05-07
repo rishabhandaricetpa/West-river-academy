3ce0dc0f95d2fc3abdd12e90edf9f2d0d7a986ff
Rebecca Risha <rebecca.risha@ithands.com>
2021-05-07 11:25:33 +0530

    changed email format for record transfer request

--- resources/views/admin/recordTransfer/sendSchoolRecord.blade.php
+++ resources/views/admin/recordTransfer/sendSchoolRecord.blade.php
@@ -1,1 +1,12 @@
-<h1>Please provide us the information of student attached below.</h1>
+<p>Hello,</p>
+<p>{{ $data['name'] }} is currently enrolled in West River Academy . A record request form is attached. Please
+    forward all
+    record to:</p>
+West River Academy <br>
+5475 S. Shawnee Way<br>
+Aurora,CO 80015<br>
+
+Fax : (949)315-4317 <br>
+Email : registrar@westriveracademy.com <br>
+
+Thank you.
