ce10b0c93426ca1110e7e93eefc5e954952c2b2a
Rebecca Risha <rebecca.risha@ithands.com>
2021-06-07 11:36:44 +0530

    sidebar fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,16 @@
 <div class="d-flex">
     <!-- * =============== Sidebar =============== * -->
-    @include('layouts.partials.sidebar')
+
     <!-- * =============== /Sidebar =============== * -->
 
     <div class="main-content position-relative ml-auto">
-        <title> @yield('pageTitle') | {{config('app.name')}}</title>
+        <title> @yield('pageTitle') | {{ config('app.name') }}</title>
         <!-- <sup>*</sup> =============== Header =============== <sup>*</sup> -->
         @include('layouts.partials.header')
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
+            @include('layouts.partials.sidebar')
             @yield('content')
         </div>
         <!-- /.content-wrapper -->
