04285a6796cf05528f215789223ac09626905445
paige.priyanka <paige.priyanka@ithands.com>
2021-08-02 15:58:19 +0530

    added student_Profile_id column in cart temp table

--- database/migrations/2021_01_11_062833_create_cart_table.php
+++ database/migrations/2021_01_11_062833_create_cart_table.php
@@ -19,8 +19,8 @@ class CreateCartTable extends Migration
             $table->unsignedBigInteger('item_id');
             $table->unsignedBigInteger('parent_profile_id');
             $table->foreign('parent_profile_id')->references('id')->on('parent_profiles')->onDelete('cascade');
+            $table->unsignedBigInteger('student_profile_id');
             $table->foreign('student_profile_id')->references('id')->on('student_profiles')->onDelete('cascade');
-
             $table->timestamps();
         });
     }
