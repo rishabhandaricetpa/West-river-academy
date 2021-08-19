dc41e662f652435547858d2ab5fd57e584b2aa99
Melissa.Manisha <melissa.manisha@ithands.com>
2021-08-19 12:01:58 +0530

    bug fixes

--- resources/views/confirm_letter_select.blade.php
+++ resources/views/confirm_letter_select.blade.php
@@ -5,8 +5,9 @@
             <main class="position-relative container form-content mt-4">
                 <div class="form-wrap border bg-light py-5 px-25 mb-4">
                     <h2 class="mb-5">What would you like to do?</h2>
-                    <div class="row confirmation-letter__options border-bottom">
-                        <h2 class="mb-3">Select your Confirmation Letter Options</h2>
+                    <h2 class="mb-3">Select your Confirmation Letter Options</h2>
+                    <div class="row confirmation-letter__options border-bottom pb-2">
+                      
                         <div id="app">
                             <list-confirmation :student='@json($student)' :gradeid='@json($grade_id)' :student_id='@json($student_id)' :confirmationdata='@json($confirmation_data)' :enrollments='@json($enrollments)' :countryData='@json($countryData)'></list-confirmation>
                         </div>
