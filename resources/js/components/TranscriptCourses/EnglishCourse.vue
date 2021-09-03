9f54814fdb2defebdec5dd821757bcb8de55ad66
Rebecca Risha <rebecca.risha@ithands.com>
2021-09-03 15:32:37 +0530

    added skip course in transcript 9-12

--- resources/js/components/TranscriptCourses/EnglishCourse.vue
+++ resources/js/components/TranscriptCourses/EnglishCourse.vue
@@ -144,6 +144,7 @@
       <button type="submit" class="btn btn-primary ml-4 float-right">
         Continue
       </button>
+      <a class="btn btn-primary float-right" @click="skipCourse()">Skip Course</a>
     </div>
   </form>
 
@@ -298,8 +299,15 @@ export default {
         }
       }
       return true;
+    },
+     skipCourse() {
+    
+   window.location =
+            "/mathematics-transcript/" +
+            this.student_id +
+            "/" +
+            this.transcript_id  ;
     }
-
 
   },
 };
