14cedd8cc6317207b78e6e3b1c07e377e835a11b
Martin Mahtab <martin.mahtab@ithands.net>
2021-09-09 09:25:11 +0530

    updated parents and student import script

--- app/Console/Commands/ImportParents.php
+++ app/Console/Commands/ImportParents.php
@@ -44,10 +44,11 @@ class ImportParents extends Command
     public function handle()
     {
         $this->line('starting import');
-        $filePath = base_path('csv/parents.csv');
+        $_this = $this;
+        $filePath = base_path('csv/family.csv');
         $reader = ReaderEntityFactory::createReaderFromFile($filePath);
         $reader->open($filePath);
-
+$duplicateCheckArray = [];
         foreach ($reader->getSheetIterator() as $sheet) {
             foreach ($sheet->getRowIterator() as
                 $rowIndex => $cells) {
@@ -56,39 +57,46 @@ class ImportParents extends Command
                     continue;
                 }
                 $p1_email = Str::of($cells[13]);
-                $user = User::where('email', $p1_email)->first();
-                if ($p1_email != '' && !is_null($user)) {
-                    if (!$user->exists()) {
-                        User::create(
-                            [
-                                'name' => $cells[14],
-                                'email' => $cells[13],
-                                'legacy_name' => $cells[11],
-                                'password' => Hash::make('12345678'),
-                            ]
-                        );
-                    } else {
-                        ParentProfile::create([
-                            'user_id' => $user ? $user->id : null,
-                            'p1_first_name' => $cells[14],
-                            'p1_last_name' => $cells[15],
-                            'p1_email' => $cells[13],
-                            'p1_cell_phone' => $cells[4],
-                            'p1_home_phone' =>  $cells[4],
-                            'street_address' => $cells[8],
-                            'legacy' => $cells[11],
-                            'city' => $cells[5],
-                            'state' =>  $cells[7],
-                            'zip_code' =>  $cells[9],
-                            'country' => $cells[6],
-                            'reference' =>  $cells[18],
-                            'immunized' =>  $cells[10]
-                        ]);
-                    }
-                }
+                if( !isset($duplicateCheckArray[$p1_email]) ) $duplicateCheckArray[$p1_email] = 0;
+                $duplicateCheckArray[$p1_email] = ($duplicateCheckArray[$p1_email] + 1);
+
+                $_this->line($p1_email);
+                // if ($p1_email != '' && 1==2) {
+
+                //     $user = User::where('email', $p1_email)->first();
+                //     if (!$user) {
+                //         $user = User::create(
+                //             [
+                //                 'name' => $cells[14],
+                //                 'email' => $cells[13],
+                //                 'legacy_name' => $cells[11],
+                //                 'password' => Hash::make('12345678'),
+                //             ]
+                //         );
+                //     } 
+                //     $parent = ParentProfile::where('p1_email', $p1_email)->first();
+                //     if( !$parent )
+                //         ParentProfile::create([
+                //             'user_id' => $user ? $user->id : null,
+                //             'p1_first_name' => $cells[14],
+                //             'p1_last_name' => $cells[15],
+                //             'p1_email' => $cells[13],
+                //             'p1_cell_phone' => $cells[4],
+                //             'p1_home_phone' =>  $cells[4],
+                //             'street_address' => $cells[8],
+                //             'legacy' => $cells[11],
+                //             'city' => $cells[5],
+                //             'state' =>  $cells[7],
+                //             'zip_code' =>  $cells[9],
+                //             'country' => $cells[6],
+                //             'reference' =>  $cells[18],
+                //             'immunized' =>  $cells[10]
+                //         ]);
+                // }
+               
             }
         }
-
+        echo '<pre>'; print_r($duplicateCheckArray);
         $reader->close();
         $this->line('import successfully');
     }
