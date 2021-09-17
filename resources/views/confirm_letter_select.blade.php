980777d01ca4fc9fa59008f889887192248157b8
Rebecca Risha <rebecca.risha@ithands.com>
2021-09-17 12:52:38 +0530

    confirmation letter changes for additional column add

--- resources/views/confirm_letter_select.blade.php
+++ resources/views/confirm_letter_select.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 @section('pageTitle', 'Confirmation')
 @section('content')
-<div class="content-wrapper">
-  <main class="position-relative container form-content mt-4">
-    <div class="form-wrap border bg-light p-5 mb-4">
-      <h2 class="mb-5">What would you like to do?</h2>
-      <h3 class="mb-3">Select your Confirmation Letter Options</h3>
-      <div class="row">
-        <div class="col-12  confirmation-letter__options border-bottom pb-2">
-          <div id="app">
-            <list-confirmation :student='@json($student)' :gradeid='@json($grade_id)' :student_id='@json($student_id)'
-              :confirmationdata='@json($confirmation_data)' :enrollments='@json($enrollments)'
-              :countryData='@json($countryData)'></list-confirmation>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
+    <div class="content-wrapper">
+        <main class="position-relative container form-content mt-4">
+            <div class="form-wrap border bg-light p-5 mb-4">
 
-  @endsection
+                <h2 class="mb-3">The following information will appear on your Confirmation of Enrollment document.</h2>
+                <div class="row">
+                    <div class="col-12  confirmation-letter__options border-bottom pb-2">
+                        <div id="app">
+                            <list-confirmation :student='@json($student)' :gradeid='@json($grade_id)'
+                                :student_id='@json($student_id)' :confirmationdata='@json($confirmation_data)'
+                                :enrollments='@json($enrollments)' :countryData='@json($countryData)'></list-confirmation>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+    @endsection
