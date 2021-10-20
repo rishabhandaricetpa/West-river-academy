ff85025619ef3358ff416447ed1f5a3ae47110b0
Rebecca Risha <rebecca.risha@ithands.com>
2021-10-20 13:15:34 +0530

    change in record transfr

--- resources/views/admin/recordTransfer/viewStudentRecord.blade.php
+++ resources/views/admin/recordTransfer/viewStudentRecord.blade.php
@@ -134,38 +134,39 @@
                                 class="btn btn-primary my-1">Send Record Request To School</button>
                             <a href="{{ route('admin.resend.request', [$studentRecord->id, $studentRecord->student_profile_id]) }}"
                                 class=" btn btn-primary my-1">Re-send Request</a>
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Send Mail</button>
+                                {{-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Send Mail</button> --}}
 
                             <a onclick="goBack()" class=" btn btn-primary my-1">Back</a>
                         </div>
-                        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog"
+                            aria-labelledby="myLargeModalLabel" aria-hidden="true">
                             <div class="modal-dialog modal-lg">
-                              <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Send Mail</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                      <span aria-hidden="true">&times;</span>
-                                    </button>
-                                  </div>
-                                  <div class="modal-body">
-                                    <form>
-                                      <div class="form-group">
-                                        <label for="recipient-name" class="col-form-label">Recipient:</label>
-                                        <input type="text" class="form-control" id="recipient-name">
-                                      </div>
-                                      <div class="form-group">
-                                        <label for="recipient-name" class="col-form-label">cc:</label>
-                                        <input type="text" class="form-control" id="recipient-name">
-                                      </div>
-                                      <div class="form-group">
-                                        <label for="message-text" class="col-form-label">Message:</label>
-                                        <textarea class="form-control" id="message-text"></textarea>
-                                      </div>
-                                    </form>
-                                  </div>
-                              </div>
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Send Mail</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                                <input type="text" class="form-control" id="recipient-name">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="recipient-name" class="col-form-label">cc:</label>
+                                                <input type="text" class="form-control" id="recipient-name">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="message-text" class="col-form-label">Message:</label>
+                                                <textarea class="form-control" id="message-text"></textarea>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
-                          </div>
+                        </div>
                         <div class="col-md-6 mt-4">
                             <table class='table-styling w-100 min-w-100' border="1">
 
