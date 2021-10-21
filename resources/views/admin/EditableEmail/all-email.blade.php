9b160e1b85f2cba9efbe24eda079a14de0d8ce19
Melissa.Manisha <melissa.manisha@ithands.com>
2021-10-21 22:16:19 +0530

    added student enrollent card and form

--- resources/views/admin/EditableEmail/all-email.blade.php
+++ resources/views/admin/EditableEmail/all-email.blade.php
@@ -2,7 +2,15 @@
 
 @section('content')
 
+<section class="content container-fluid  mt-3 pb-3">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <!-- first card parent details -->
 
-
+    <div class="card mt-3 px-4 py-5">
+        <h2 class="py-4">Student Enrollement Mail</h2>
+        <a class="email-card m-1 p-3" href="#">Student Enrollement Mail</a>
+        <a class="email-card m-1 p-3" href="#">Student Enrollement Mail</a>
+    </div>
+</section>
 
 @endsection
--- resources/views/admin/EditableEmail/edit-email.blade.php
+++ resources/views/admin/EditableEmail/edit-email.blade.php
@@ -2,7 +2,28 @@
 
 @section('content')
 
+<section class="content container-fluid  mt-3 pb-3">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <!-- first card parent details -->
 
+    <div class="card mt-3 px-4 py-5">
+        <form>
+            <div class="form-group">
+              <label for="Tittle" class="col-form-label">Tittle:</label>
+              <input type="text" class="form-control" id="Tittle">
+            </div>
+            <div class="form-group">
+                <label for="recipient-name" class="col-form-label">Recipient:</label>
+                <input type="text" class="form-control" id="recipient-name">
+              </div>
+            <div class="form-group">
+              <label for="message-text" class="col-form-label">Message:</label>
+              <textarea class="form-control" id="message-text"></textarea>
+            </div>
+            <button class="btn btn-primary">Submit</button>
+          </form>
+    </div>
+</section>
 
 
 @endsection
