1661df9207dd5c1165b8d250207b365dcce18c4d
Rebecca Risha <rebecca.risha@ithands.com>
2021-10-21 13:06:34 +0530

    format date in record

---
+++ resources/views/admin/recordTransfer/sample-record.blade.php
@@ -0,0 +1,12 @@
+<p>Hello,</p>
+<p>{{ $student_name }} is currently enrolled in West River Academy . A record request form is attached. Please
+    forward all
+    record to:</p>
+West River Academy <br>
+5475 S. Shawnee Way<br>
+Aurora,CO 80015<br>
+
+Fax : (949)315-4317 <br>
+Email : registrar@westriveracademy.com <br>
+
+Thank you.
