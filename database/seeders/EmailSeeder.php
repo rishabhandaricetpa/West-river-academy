352bd20b97f43b4e2e9f30b2a2c58260602eb8ff
Rebecca Risha <rebecca.risha@rubicotech.in>
2021-11-17 09:21:15 +0530

    change in email seeder

--- database/seeders/EmailSeeder.php
+++ database/seeders/EmailSeeder.php
@@ -18,22 +18,22 @@ class EmailSeeder extends Seeder
         $data = [
             ['type' => 'enrollment', 'content' => '<p>Hi,</p>
 
-<p><b></b>{{ $student_name }}<b></b>&nbsp; &nbsp;has successfully enrolled from&nbsp; {{ $enrollment_start_date }}&nbsp; to {{ $enrollment_end_date }}.</p>
+<p><b></b>student_name<b></b>&nbsp; &nbsp;has successfully enrolled from&nbsp; enrollment_start_date &nbsp; to enrollment_end_date .</p>
 <p>Thank You!</p>
 <p></p>'],
             ['type' => 'graduation', 'content' => '<h4>Hi,</h4>
 
 <p><b>We have reviewed your application to graduate and approved it. Please log in to your account and pay the
         Graduation
-        Fee of ${{    $total_fees }}, which includes the Graduation Project, high school diploma.</b></p>
+        Fee of $total_fees , which includes the Graduation Project, high school diploma.</b></p>
 
 <p><b>We look forward to working with you and helping you graduate from West River Academy.&nbsp;</b></p>
 <p><b>Thank You!&nbsp;</b></p>'],
             ['type' => 'moneygram', 'content' => '<h3>MoneyGram Payment</h3>
 
-<h3>Hi {{     $user_name }},</h3>
+<h3>Hi user_name ,</h3>
 <p>Thank you for your order. To pay the total of
-    ${{     $amount }} by MoneyGram, please log in to your online account to open the web page with MoneyGram
+    $amount  by MoneyGram, please log in to your online account to open the web page with MoneyGram
     payment instructions.
     <a target="_blank" rel="nofollow" href="http://westriveracademy.test/moneygram-transfer">http://westriveracademy.test/moneygram-transfer</a>
 </p>
@@ -47,7 +47,7 @@ class EmailSeeder extends Seeder
 <a target="_blank" rel="nofollow">The West River Academy Team</a><br>'],
             ['type' => 'moneyorder', 'content' => '<h3>West River Academy - Check Or Money Order Instructions
     </h3>
-    <p>Hi {{  $user_name }} ,</p>
+    <p>Hi user_name  ,</p>
     <b></b>Thank<b></b> you for your order. To
         pay by check or money order, please:<br> 
     <p> 1. Make your check or money
@@ -72,9 +72,9 @@ class EmailSeeder extends Seeder
         <a target="_blank" rel="nofollow">The West River Academy Team</a><br>'],
             ['type' => 'banktransfer', 'content' => '<h3>West River Academy - Bank Transfer Instructions</h3>
 
-<p>Hi {{   $user_name }},</p>
+<p>Hi user_name ,</p>
 <p>Thank you for your order. To pay the total of
-    ${{   $amount }} by Bank Transfer, please log in to your online account to open the web page with Bank Transfer
+    $amount  by Bank Transfer, please log in to your online account to open the web page with Bank Transfer
     instructions.
 
 </p><p>We will notify you by email and in your account
