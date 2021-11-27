f1c5124860d6f93b9f11c68b5a71b6467dfabcf9
Rebecca Risha <rebecca.risha@rubicotech.in>
2021-11-27 21:17:23 +0530

    header bug resolved in admin student

--- resources/views/admin/familyInformation/view-student.blade.php
+++ resources/views/admin/familyInformation/view-student.blade.php
@@ -1,45 +1,45 @@
 @extends('admin.app')
 
 @section('content')
-<!-- Content Header (Page header) -->
-<div class="content-header">
-  <div class="container-fluid position-relative">
-    <h1>Student List</h1>
-    <div class="d-flex">
-      <ol class="breadcrumb ml-auto">
-        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.notification') }}">Home</a></li>
-        <li class="breadcrumb-item active">Student List</li>
-      </ol>
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid position-relative">
+            <h1>Student List</h1>
+            <div class="d-flex">
+                <ol class="breadcrumb ml-auto">
+                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.notification') }}">Home</a></li>
+                    <li class="breadcrumb-item active">Student List</li>
+                </ol>
+            </div>
+        </div><!-- /.container-fluid -->
     </div>
-  </div><!-- /.container-fluid -->
-</div>
-<!-- /.content-header -->
-<div class="card m-3 family-details ">
-  <div class="sticky mb-2 pb-1">
-    @include('admin.familyInformation.student_header')
+    <!-- /.content-header -->
+    <div class="card m-3 family-details ">
+        <div class="sticky mb-2 pb-1">
+            @include('admin.familyInformation.parent_header')
 
-  </div>
-  <table id="student-table" class="table table-bordered table-striped data-table"">
-            <thead>
-            <tr>
-              <th>Student</th>
-              <th>DOB</th>
-              <th>Gender</th>
-              <th>Country</th>
-              <th>Email</th>
-              <th>Action</th>
-              <th>Payment</th>
-              <th>Transcript</th>
-              <th>Graduation</th>
-              <th>Record Transfer</th>
-              <th>View Uploaded Document</th>
-               <th>Upload Documents</th>
-            </tr>
-            </thead>
-            <tbody>
-           
-          </tbody>
-      </table>
-      </div>
-    <!-- /.content -->
+        </div>
+        <table id="student-table" class="table table-bordered table-striped data-table"">
+                <thead>
+                <tr>
+                  <th>Student</th>
+                  <th>DOB</th>
+                  <th>Gender</th>
+                  <th>Country</th>
+                  <th>Email</th>
+                  <th>Action</th>
+                  <th>Payment</th>
+                  <th>Transcript</th>
+                  <th>Graduation</th>
+                  <th>Record Transfer</th>
+                  <th>View Uploaded Document</th>
+                   <th>Upload Documents</th>
+                </tr>
+                </thead>
+                <tbody>
+               
+              </tbody>
+          </table>
+          </div>
+        <!-- /.content -->
     @endsection
